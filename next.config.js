deafe08a9e2ed06a24d82cb00a5d54e4c51249f7
fiorellatah61 <fiorellatah6.1@gmail.com>
2025-09-14 18:17:43 -0500

    m

--- next.config.js
+++ next.config.js
@@ -1,21 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true, // Agrega esta línea también
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '*.supabase.co',
-        port: '',
-        pathname: '/storage/v1/object/public/**',
-      },
-    ],
-  },
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true, // Agrega esta línea también
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: '*.supabase.co',
+//         port: '',
+//         pathname: '/storage/v1/object/public/**',
+//       },
+//     ],
+//   },
+// };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
